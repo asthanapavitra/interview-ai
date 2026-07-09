b0ad554e8b5757ce3d73a054829a30b5a77d1718
Pavitra Asthana <asthanapavitra@gmail.com>
2026-07-09 21:30:09 +0530

    Changed browser launch code

--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -138,7 +138,13 @@ async function generateInterviewReport({
   }
 }
 async function generatePDFfromHtml(htmlContent) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+  ],
+});
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
 
